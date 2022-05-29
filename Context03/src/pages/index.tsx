57f139f1e1a08c9f8f11cd079065c3f8330926aa
f-toono <fumiya.toono@relic.co.jp>
2022-05-30 07:45:33 +0900

    基盤

---
+++ Context03/src/pages/index.tsx
@@ -0,0 +1,43 @@
+import type { NextPage } from "next";
+import { useContext } from "react";
+import { Todo } from "src/types";
+import { TodoContext } from "./_app";
+
+// ジェネリクス
+const Home: NextPage = () => {
+  const { todos, setTodos } = useContext(TodoContext);
+
+  const toggleIsDone = (id: Todo["id"]) => {
+    setTodos((prevTodos) => {
+      return prevTodos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, isDone: !todo.isDone };
+          // isDone以外はそのまま渡す
+        }
+        return todo;
+      });
+    });
+  };
+  return (
+    <div>
+      <h3>TODO一覧</h3>
+      {todos.map((todo) => {
+        return (
+          <div key={todo.id}>
+            <label style={{ fontSize: "2rem" }}>
+              <input
+                type="checkbox"
+                checked={todo.isDone}
+                onChange={() => toggleIsDone(todo.id)}
+                style={{ width: "1.5rem", height: "1.5rem" }}
+              />
+              {todo.text}
+            </label>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Home;
